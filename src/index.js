16b78e1cc4919ba3c14b45b2a31a2224011c3a68
Eduard <sorokolietov@gmail.com>
2023-09-17 10:13:59 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -2,55 +2,48 @@
 // 2. PER_PAGE ?? як можна по іншому отримати доступ до per_page
 // 3. де обробляти клас кнопки??
 
+import { makeRequest } from "./js/pixabay-api";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
 const formRef = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
-const buttonMore = document.querySelector('.load-more');
-
-const API_KEY = '39437968-8994b31ccac94168d8d24ad3e';
-const BASE_URL = 'https://pixabay.com/api/';
-const PER_PAGE = 40;
+export const buttonMore = document.querySelector('.load-more');
 
 let page = 1;
 let query = '';
 
-const fetchQuery = (page, query) => {
-
-  const params = new URLSearchParams({
-    key: API_KEY,
-    q: query,
-    per_page: PER_PAGE,
-    page,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
-  
-  return fetch(`${BASE_URL}?${params.toString()}`).then(res => {
-    
-    if (!res.ok) throw new Error(res.statusText);
-
-    return res.json();
-  }).then(res => {
-
-    buttonMore.classList.remove('is-hidden');
-
-    if (res.totalHits < PER_PAGE * page) {
-      buttonMore.classList.add('is-hidden');
-    }
-
-    return res;
-  })
-};
-
-function loadMoreHandler() {
+export async function loadMoreHandler() {
   page += 1;
   buttonMore.classList.add('is-hidden');
-  fetchQuery(page, query).then((res) => render(res.hits, gallery));
-}
+
+  const request = await makeRequest(page, query);
+  render(request.hits, gallery);
+
+  if (page === 13) {
+    Notify.info("We're sorry, but you've reached the end of search results.")
+  }
+};
+
+async function formHandler(e) {
+  e.preventDefault();
+
+  page = 1;
+  query = e.currentTarget.searchQuery.value;
+
+  const request = await makeRequest(page, query);
+
+  if (!request.hits.length) {
+    Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+  } else {
+    Notify.success(`Hooray! We found ${request.totalHits} images.`);
+  }
+
+  render(request.hits, gallery);
+};
 
 function render(arr, container) {
   const markup = arr.map(item => `<div class="photo-card">
-    <img src="${item.previewURL}" alt="${item.tags}" loading="lazy" />
+    <img src="${item.webformatURL}" width="300" alt="${item.tags}" loading="lazy" />
     <div class="info">
       <p class="info-item">
         <b>Likes</b> ${item.likes}
@@ -78,21 +71,11 @@ function render(arr, container) {
   .querySelector(".gallery")
   .firstElementChild.getBoundingClientRect();
 
-window.scrollBy({
-  top: cardHeight * 2,
-  behavior: "smooth",
-});
-}
-
-function formHandler(e) {
-  e.preventDefault();
-
-  page = 1;
-  query = e.currentTarget.searchQuery.value;
-
-  fetchQuery(page, query).then((res) => render(res.hits, gallery));
-}
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: "smooth",
+  });
+};
 
 formRef.addEventListener('submit', formHandler);
-
 buttonMore.addEventListener('click', loadMoreHandler);
--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,17 +1,37 @@
-
+import axios from "axios";
+import { buttonMore, loadMoreHandler } from "../index.js";
 
 const API_KEY = '39437968-8994b31ccac94168d8d24ad3e';
 const BASE_URL = 'https://pixabay.com/api/';
+const PER_PAGE = 40;
 
-const options = {
+export async function makeRequest(page, query) {
+
+  const params = new URLSearchParams({
     key: API_KEY,
-    q: null,
+    q: query,
+    per_page: PER_PAGE,
+    page,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: true,
+  });
+
+  try {
+    const request = await axios.get(`${BASE_URL}?${params}`);
+
+    buttonMore.classList.remove('is-hidden');
+
+    console.log(request.data.totalHits, PER_PAGE * page);
+    
+    if (request.data.totalHits < PER_PAGE * page) {
+      buttonMore.removeEventListener('click', loadMoreHandler);
+      buttonMore.classList.add('is-hidden');
+    }
+    
+    return request.data;
+  } catch (error) {
+    console.error(error);
+  }
 };
-
-fetch(`${BASE_URL}?key=${API_KEY}q=cat&page=1`).then(console.log);
-
-console.log('asd');
 
